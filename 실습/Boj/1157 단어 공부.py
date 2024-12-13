c4ff69b10e286bbdf8d7c3e0a6868362e39b2540
hyunjiji0802 <guswl4174@naver.com>
2024-12-14 04:10:21 +0900

    1214 코데 문제 업뎃

--- 실습/Boj/1157 단어 공부.py
+++ 실습/Boj/1157 단어 공부.py
@@ -1,17 +1,25 @@
-word = str(input()).upper()
-dic = {}
-for s in word:
-    if s in dic:
-        dic[s]+=1
-    else:
-        dic[s] = 1
+# word = str(input()).upper()
+# dic = {}
+# for s in word:
+#     if s in dic:
+#         dic[s] += 1
+#     else:
+#         dic[s] = 1
+#
+# #정답, 최대값 초기화
+# max_num = 0
+# answer = ''
+# for k, v in dic.items():
+#     if max_num < v:
+#         max_num = v
+#         answer = k
+#     elif max_num != 0 and max_num == v: #동일하면
+#         answer = '?'
+# print(answer)
 
-max_num = 0
-answer = ''
-for k, v in dic.items():
-    if max_num < v:
-        max_num = v
-        answer = k
-    elif max_num != 0 and max_num == v: #동일하면
-        answer = '?'
-print(answer)
+from collections import Counter
+
+word = str(input()).upper()
+word_counts = Counter(word)
+print(word_counts)
+
